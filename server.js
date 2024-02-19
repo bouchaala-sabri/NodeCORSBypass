f5e1a414cd5e3ee69f015331e852f175c0316eb4
Sabri Bouchaala <bouchaala.sabri@gmail.com>
2024-02-19 06:43:37 +0100

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.get('/:url', async (req, res) => {
         const url = req.params.url;
         const response = await axios.get(url, {
             headers: {
-                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36'
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36 Edg/121.0.0.0'
             }
         });
         res.send(response.data);
