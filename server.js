a808b0092ff7de51ce8655730880ffd50b2dd969
Sabri Bouchaala <bouchaala.sabri@gmail.com>
2024-03-04 14:21:50 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,8 @@
 const express = require('express');
-const cors = require('cors');
 const axios = require('axios');
+const cheerio = require('cheerio');
 
 const app = express();
-
-app.use(cors());
 
 app.get('/:url', async (req, res) => {
     try {
@@ -14,7 +12,19 @@ app.get('/:url', async (req, res) => {
                 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36 Edg/121.0.0.0'
             }
         });
-        res.send(response.data);
+
+        const $ = cheerio.load(response.data);
+        $('a[href^="/"], img[src^="/"]').each(function() {
+            const oldUrl = $(this).attr('href') || $(this).attr('src');
+            const newUrl = new URL(oldUrl, url).href;
+            if ($(this).attr('href')) {
+                $(this).attr('href', newUrl);
+            } else {
+                $(this).attr('src', newUrl);
+            }
+        });
+
+        res.send($.html());
     } catch (error) {
         res.status(500).send({ 
             message: 'Error fetching url',
@@ -26,7 +36,6 @@ app.get('/:url', async (req, res) => {
         });
     }
 });
-
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Server running on port ${port}`));
